0a2a10436c6d56de4052b652848975a3864cd248
Alishba Basharat <166277441+AlishbaBasharat@users.noreply.github.com>
2024-04-20 13:08:39 -0700

    created ATM machine

---
+++ index.ts
@@ -0,0 +1,58 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let mybalance = 10000;
+let mypin = 12345;
+let myaddress = "main branch in  Gulshan_iqbal"
+
+let pinanswer = await inquirer.prompt([
+{  
+    name: "pin",
+    type: "number",
+    message: "Enter your pin"
+},
+])
+if (pinanswer.pin === mypin) {
+     console.log("you can do further process");
+     
+    let actionanswer = await inquirer.prompt(
+        [
+            {
+
+        name: 'action',
+        type: 'list',
+        message: "select any one of them",
+        choices: ["withdraw", "check balance", "ordering checkbook"],
+    }
+]
+);
+if (actionanswer.action === "withdraw") {
+    let operationanswer = await inquirer.prompt(
+        [
+        {
+        name: "operation",
+        type: "number",
+        message: "how much you want to withdraw",
+    }
+]
+);  console.log("message");
+
+    if (operationanswer > mybalance){
+        console.log("sorry! Insuficient amount");
+        
+    }
+    
+} 
+else if (actionanswer.action === "check balance"){
+    console.log(`your balance is: ${ mybalance}`); 
+} 
+else if (actionanswer.action === "ordering checkbook") {
+    console.log(`please visit the bank ${myaddress}`);
+    
+}
+
+}
+else{
+    console.log("Incorrect pin You can not do any further process");
+    
+}
