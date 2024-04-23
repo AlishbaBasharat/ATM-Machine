f65fc2567772f15000688f557dcb234f410c24e6
Alishba Basharat <166277441+AlishbaBasharat@users.noreply.github.com>
2024-04-23 04:23:38 -0700

    created atm machine

--- index.js
+++ index.js
@@ -2,23 +2,23 @@
 import inquirer from "inquirer";
 let mybalance = 10000;
 let mypin = 12345;
-let myaddress = "main branch in  Gulshan_iqbal";
+let myaddress = "Main branch in Gulshan_iqbal";
 let pinanswer = await inquirer.prompt([
     {
         name: "pin",
         type: "number",
-        message: "Enter your pin"
+        message: "Enter your pin",
     },
 ]);
 if (pinanswer.pin === mypin) {
     console.log("you can do further process");
     let actionanswer = await inquirer.prompt([
         {
-            name: 'action',
-            type: 'list',
+            name: "action",
+            type: "list",
             message: "select any one of them",
             choices: ["withdraw", "check balance", "ordering checkbook"],
-        }
+        },
     ]);
     if (actionanswer.action === "withdraw") {
         let operationanswer = await inquirer.prompt([
@@ -26,11 +26,14 @@ if (pinanswer.pin === mypin) {
                 name: "operation",
                 type: "number",
                 message: "how much you want to withdraw",
-            }
+            },
         ]);
-        console.log("message");
-        if (operationanswer > mybalance) {
+        if (operationanswer.operation >= mybalance) {
             console.log("sorry! Insuficient amount");
+        }
+        if (operationanswer.operation <= mybalance) {
+            mybalance = mybalance - operationanswer.operation;
+            console.log("my balance:", mybalance);
         }
     }
     else if (actionanswer.action === "check balance") {
--- index.ts
+++ index.ts
@@ -3,56 +3,56 @@ import inquirer from "inquirer";
 
 let mybalance = 10000;
 let mypin = 12345;
-let myaddress = "main branch in  Gulshan_iqbal"
+let myaddress = "Main branch in Gulshan_iqbal";
 
 let pinanswer = await inquirer.prompt([
-{  
+  {
     name: "pin",
     type: "number",
-    message: "Enter your pin"
-},
-])
-if (pinanswer.pin === mypin) {
-     console.log("you can do further process");
-     
-    let actionanswer = await inquirer.prompt(
-        [
-            {
+    message: "Enter your pin",
+  },
+]);
 
-        name: 'action',
-        type: 'list',
-        message: "select any one of them",
-        choices: ["withdraw", "check balance", "ordering checkbook"],
-    }
-]
-);
-if (actionanswer.action === "withdraw") {
-    let operationanswer = await inquirer.prompt(
-        [
-        {
+
+if (pinanswer.pin === mypin) {
+
+  console.log("you can do further process");
+
+  let actionanswer = await inquirer.prompt([
+    {
+      name: "action",
+      type: "list",
+      message: "select any one of them",
+      choices: ["withdraw", "check balance", "ordering checkbook"],
+    },
+  ]);
+
+
+  if (actionanswer.action === "withdraw") {
+
+    let operationanswer = await inquirer.prompt([
+      {
         name: "operation",
         type: "number",
         message: "how much you want to withdraw",
+      },
+    ])
+    if (operationanswer.operation >= mybalance){
+      console.log("sorry! Insuficient amount");
     }
-]
-);  console.log("message");
 
-    if (operationanswer > mybalance){
-        console.log("sorry! Insuficient amount");
-        
+    if (operationanswer.operation <= mybalance) {
+      mybalance = mybalance - operationanswer.operation;
+      console.log("my balance:", mybalance);
     }
-    
-} 
-else if (actionanswer.action === "check balance"){
-    console.log(`your balance is: ${ mybalance}`); 
-} 
-else if (actionanswer.action === "ordering checkbook") {
+
+
+
+  } else if (actionanswer.action === "check balance") {
+    console.log(`your balance is: ${mybalance}`);
+  } else if (actionanswer.action === "ordering checkbook") {
     console.log(`please visit the bank ${myaddress}`);
-    
-}
-
-}
-else{
-    console.log("Incorrect pin You can not do any further process");
-    
+  }
+} else {
+  console.log("Incorrect pin You can not do any further process");
 }
